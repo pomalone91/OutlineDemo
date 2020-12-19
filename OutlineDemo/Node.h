2e75bd85d2adce454fcd5f7a17f43744c6cd1d4b
pomalone91 <pomalone91@gmail.com>
2020-12-19 11:33:04 -0700

    Initial commit

---
+++ OutlineDemo/Node.h
@@ -0,0 +1,13 @@
+//
+//  Node.h
+//  OutlineDemo
+//
+//  Created by Paul Malone on 12/19/20.
+//  Copyright © 2020 Paul Malone. All rights reserved.
+//
+
+#ifndef Node_h
+#define Node_h
+
+
+#endif /* Node_h */
---
+++ OutlineDemo/Node.m
@@ -0,0 +1,9 @@
+//
+//  Node.m
+//  OutlineDemo
+//
+//  Created by Paul Malone on 12/19/20.
+//  Copyright © 2020 Paul Malone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
